36dc81a7f737165e57947a0c95d850d8b6c5d216
DLitvinenko <dimaboblitvinenko@gmail.com>
2023-03-21 13:30:32 +0300

    ProgrammWorking

--- FinalWorkFirstQuater/FinalWorkFirstQuater/Program.cs
+++ FinalWorkFirstQuater/FinalWorkFirstQuater/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося
+//массива строк формирует новый массив из строк,
+//длина которых меньше, либо равна 3 символам.
+//Первоначальный массив можно ввести с клавиатуры,
+//либо задать на старте выполнения алгоритма. При
+//решении не рекомендуется пользоваться коллекциями,
+//лучше обойтись исключительно массивами.
+using static MetodsForProgramm.Metods;
+
+namespace FinalWorkForQuater
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            string[] array = CreateArray(Input("Введите размер массива: "));
+            Filling(array);
+            Console.WriteLine(Printing(array));
+            if (array.Length > 3)
+            {
+                array = NewArrayFillingOld(array);
+                Console.WriteLine(Printing(array));
+            }
+            else Console.WriteLine("Заданный массив в пределах целевого размера.");
+        }
+    }
+}
---
+++ FinalWorkFirstQuater/MetodsForProgramm/Metods.cs
@@ -0,0 +1,49 @@
+﻿namespace MetodsForProgramm
+{
+    public class Metods
+    {
+        public static string[] CreateArray(int size)
+        {
+            return new string[size];
+        }
+
+        public static int Input (string msg)
+        {
+            Console.Write(msg);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void Filling(string[]array)
+        {
+            int size = array.Length;
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите элемент массива номер {i+1}: ");
+                array[i] = Console.ReadLine();
+            }
+        }
+
+        public static string Printing(string[] array)
+        {
+            int size = array.Length;
+            string result = String.Empty;
+            for(int i = 0;i < size;i++)
+            {
+                result += $"{array[i]} ";
+            }
+            return result;
+        }
+
+        public static string[] NewArrayFillingOld(string []array, int min = 0, int max = 3)
+        {
+            string[] resultArray = CreateArray(new Random().Next(min, max)); 
+            int size = resultArray.Length;
+            for(int i = 0; i<size;i++)
+            {
+                resultArray[i] = array[i];
+            
+            }
+            return resultArray;
+        }
+    }
+}
